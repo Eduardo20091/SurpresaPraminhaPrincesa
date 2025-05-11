e0cd39580c9de9f5e89c59f625f5c7021c32683f
Eduardo20091 <eduardofleis121@gmail.com>
2025-05-10 20:11:19 -0400

    Create Surpresa.py

---
+++ Surpresa.py
@@ -0,0 +1,17 @@
+import time
+import webbrowser
+
+# Mensagem de introdução
+print("Oi, minha princesa! 😊")
+time.sleep(2)
+print("Eu preparei uma surpresinha pra você! ❤️")
+time.sleep(2)
+
+# Abrindo um link especial no navegador
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # Troque pelo link que você quiser
+print("Abrindo algo especial para você... 😏")
+time.sleep(3)
+webbrowser.open(url)
+
+# Mensagem final
+print("Espero que você goste! Te amo muito! 💖")
